1578b140b5afe9ebdf2495d03154948e01e6b617
Vinycius <144926455+External301@users.noreply.github.com>
2023-10-25 08:57:49 -0300

    a

--- roblox.lua
+++ roblox.lua
@@ -1,6 +1,11 @@
 local plr = game.Players.LocalPlayer
 local plrs = game.Players
 local char = plr.Character
+local HumRp = char:WaitForChild("HumanoidRootPart")
+
+local Configs = {
+    AutoFarmMob = false,
+}
 
 local function GetMob()
     local currentMob = nil
@@ -8,4 +13,19 @@ local function GetMob()
         currentMob = v
     end
     return currentMob
+end
+
+while true do task.wait()
+task.spawn(function()
+    if AutoFarmMob then
+        local Mob = GetMob()
+        if Mob and Mob:FindFirstChild("HumanoidRootPart") and HumRp then
+            HumRp.CFrame = Mob:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0,0,-3)
+        end
+    end
+end)
+
+
+
+
 end
