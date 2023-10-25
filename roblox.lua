6ca3c0b20f41ba22c1d094d5b460b9abc5e00561
Vinycius <144926455+External301@users.noreply.github.com>
2023-10-25 09:02:47 -0300

    a

--- roblox.lua
+++ roblox.lua
@@ -2,6 +2,9 @@ local plr = game.Players.LocalPlayer
 local plrs = game.Players
 local char = plr.Character
 local HumRp = char:WaitForChild("HumanoidRootPart")
+local MobFolder = workspace:WaitForChild("Mobs")
+
+getgenv().Farm = false
 
 local Configs = {
     AutoFarmMob = false,
@@ -9,22 +12,25 @@ local Configs = {
 
 local function GetMob()
     local currentMob = nil
-    for i,v in workspace.Mobs:GetChildren() do
+    for i,v in MobFolder:GetChildren() do
         currentMob = v
     end
     return currentMob
 end
 
 while true do task.wait()
+--//MobFarm
 task.spawn(function()
-    if AutoFarmMob then
+    if AutoFarmMob or getgenv().Farm then
         local Mob = GetMob()
         if Mob and Mob:FindFirstChild("HumanoidRootPart") and HumRp then
             HumRp.CFrame = Mob:WaitForChild("HumanoidRootPart").CFrame * CFrame.new(0,0,-3)
         end
     end
 end)
+task.spawn(function()
 
+end)
 
 
 
